6a2f7e1f750c0ef8f41ff70b52629eb24d3c143e
codingacademypth <techexergy@gmail.com>
2023-05-19 17:59:32 +0530

    calculator

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(){
+let a=parseInt(prompt("ENter the no: "))
+let b=parseInt(prompt("ENter the no: "))
+document.write(a+b,"<br>");
+}
+function sub(){
+    let a=prompt("ENter the no: ")
+let b=prompt("ENter the no: ")
+    document.write(a-b,"<br>");
+}
+function multiply(){
+    let a=prompt("ENter the no: ")
+let b=prompt("ENter the no: ")
+    document.write(a*b,"<br>");
+}
+function divide(){
+    let a=prompt("ENter the no: ")
+let b=prompt("ENter the no: ")
+    document.write(a/b,"<br>");
+}
